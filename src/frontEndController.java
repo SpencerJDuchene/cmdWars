48d86ba9e93a8a057ce922798881517e2c308f1d
SpencerJDuchene <work@spencerduchene.com>
2016-10-28 13:14:19 -0700

    Gui implemented
    Gui has been attached to the code so it can now run gui upon using the
    final .jar file. Currently has nothing more than containers and buttons

--- src/frontEndController.java
+++ src/frontEndController.java
@@ -1,34 +1,27 @@
 /**
  * Created by lostw on 10/28/2016.
  */
-/**
- * Sample Skeleton for 'ComplexApplication_css.fxml' Controller Class
- */
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
-public class frontEndController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+public class frontEndController
+        implements Initializable {
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+    @FXML //  fx:id="myButton"
+    private Button myButton; // Value injected by FXMLLoader
 
-    @FXML // fx:id="x3"
-    private Font x3; // Value injected by FXMLLoader
 
-    @FXML // fx:id="x4"
-    private Color x4; // Value injected by FXMLLoader
+    @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        assert myButton != null : "fx:id=\"myButton\" was not injected: check your FXML file 'ComplexApplication_css.fxml'.";
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert x3 != null : "fx:id=\"x3\" was not injected: check your FXML file 'ComplexApplication_css.fxml'.";
-        assert x4 != null : "fx:id=\"x4\" was not injected: check your FXML file 'ComplexApplication_css.fxml'.";
+        // initialize your logic here: all @FXML variables will have been injected
 
     }
+
 }
--- src/main.java
+++ src/main.java
@@ -1,14 +1,35 @@
-import javafx.application.Application;
-
 /**
  * Created by lostw on 10/28/2016.
  */
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class main {
+public class Main extends Application {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
+        Application.launch(Main.class, (java.lang.String[])null);
     }
 
-    
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            VBox page = (VBox) FXMLLoader.load(Main.class.getResource("ComplexApplication_css.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("FXML is Simple");
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
